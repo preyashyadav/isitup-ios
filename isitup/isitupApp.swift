32be7d5a8c41fecd0d31d5691906f034d78bc7a0
preyashyadav <preyadav@ucdavis.edu>
2026-01-04 06:15:16 -0800

    Initial Commit

---
+++ isitup/isitupApp.swift
@@ -0,0 +1,17 @@
+//
+//  isitupApp.swift
+//  isitup
+//
+//  Created by Preyash Yadav on 1/4/26.
+//
+
+import SwiftUI
+
+@main
+struct isitupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
